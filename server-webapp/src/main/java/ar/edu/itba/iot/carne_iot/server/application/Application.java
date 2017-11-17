f0b2885d5c60df83635c001d5ca8e1b572ef398f
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-17 06:52:27 -0300

    Add Spring Hateaos
    - This dependency has Spring-WebMVC as a transitive dependency, so we add exclusion of autoconfiguration for
    	- WebMvc
    	- DispatcherServlet
    	- ErrorMvc

--- server-webapp/src/main/java/ar/edu/itba/iot/carne_iot/server/application/Application.java
+++ server-webapp/src/main/java/ar/edu/itba/iot/carne_iot/server/application/Application.java
@@ -2,12 +2,21 @@ package ar.edu.itba.iot.carne_iot.server.application;
 
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Bootstrap class.
  */
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                WebMvcAutoConfiguration.class,
+                DispatcherServletAutoConfiguration.class,
+                ErrorMvcAutoConfiguration.class,
+        }
+)
 public class Application {
 
     public static void main(String[] args) {
