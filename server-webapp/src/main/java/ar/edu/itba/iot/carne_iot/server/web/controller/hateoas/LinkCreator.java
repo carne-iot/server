d152a50d071de1356ce0e54d6d4e845439cef1de
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-11-18 19:00:40 -0300

    Add owner rel for Registered Devices
    - Add methods to LinkCreator to support adding relations

--- server-webapp/src/main/java/ar/edu/itba/iot/carne_iot/server/web/controller/hateoas/LinkCreator.java
+++ server-webapp/src/main/java/ar/edu/itba/iot/carne_iot/server/web/controller/hateoas/LinkCreator.java
@@ -36,13 +36,42 @@ public class LinkCreator {
      */
     public static <T> Link createSelfLink(T entity, Function<T, ?> getIdFunction, Class<?> controllerClass,
                                           Object... parameters) {
+        return createLink(entity, getIdFunction, Link.REL_SELF, controllerClass, parameters);
+    }
+
+    /**
+     * Creates a {@link Link} for a given reference (i.e relation is an argument).
+     *
+     * @param entity          The entity whose {@link Link} must be retrieved.
+     * @param getIdFunction   The {@link Function} that generates an identification from the {@code entity}.
+     * @param controllerClass The {@link Class} of the controller in charge of the given {@code entity}.
+     * @param <T>             The concrete type of the entity.
+     * @return The created link.
+     */
+    public static <T> Link createLink(T entity, Function<T, ?> getIdFunction, String rel, Class<?> controllerClass) {
+        return createLink(entity, getIdFunction, rel, controllerClass, new Object[0]);
+    }
+
+    /**
+     * Creates a {@link Link} for a given reference (i.e relation is an argument).
+     *
+     * @param entity          The entity whose {@link Link} must be retrieved.
+     * @param getIdFunction   The {@link Function} that generates an identification from the {@code entity}.
+     * @param controllerClass The {@link Class} of the controller in charge of the given {@code entity}.
+     * @param parameters      An array of {@link Object} for expanding the uri template declared in the controller.
+     * @param <T>             The concrete type of the entity.
+     * @return The created link.
+     */
+    public static <T> Link createLink(T entity, Function<T, ?> getIdFunction, String rel, Class<?> controllerClass,
+                                      Object... parameters) {
         return JaxRsLinkBuilder.linkTo(controllerClass, parameters)
                 .slash(getIdFunction.apply(entity))
-                .withSelfRel();
+                .withRel(rel);
     }
 
     /**
      * Creates a {@link URI} for a self reference.
+     * Method to be used for creating the URI for the 'Location' header.
      *
      * @param entity          The entity whose {@link Link} must be retrieved.
      * @param getIdFunction   The {@link Function} that generates an identification from the {@code entity}.
@@ -56,6 +85,7 @@ public class LinkCreator {
 
     /**
      * Creates a {@link URI} for a self reference.
+     * Method to be used for creating the URI for the 'Location' header.
      *
      * @param entity          The entity whose {@link Link} must be retrieved.
      * @param getIdFunction   The {@link Function} that generates an identification from the {@code entity}.
@@ -70,5 +100,4 @@ public class LinkCreator {
 
         return URI.create(link.getHref());
     }
-
 }
